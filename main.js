179d91dadf06dbbcdc4648a23e48f34de1be63d1
Underscore96 <ottonelloiisabella@gmail.com>
2023-07-17 02:37:35 +0200

    cc

--- main.js
+++ main.js
@@ -6,16 +6,39 @@ DBService.getBreweries(pageNumber)
 
 
 
-function render(breweries) {
-        const breweriesContainer = document.getElementById('breweries-container');
-        breweriesContainer.innerHTML = ''; // Clear the existing breweries
+// function render(breweries) {
+//         const breweriesContainer = document.getElementById('breweries-container');
+//         breweriesContainer.innerHTML = ''; // Clear the existing breweries
       
+//         breweries.forEach(brewery => {
+//           const breweryElement = document.createElement('div');
+//           breweryElement.textContent = brewery.name;
+//           breweriesContainer.appendChild(breweryElement);
+//         });
+    //  }
+
+     function render(breweries) {
+        const breweriesList = document.querySelector('.breweries-list');
+      
+        // Clear the existing content
+        breweriesList.innerHTML = '';
+      
+        // Generate card for each brewery
         breweries.forEach(brewery => {
-          const breweryElement = document.createElement('div');
-          breweryElement.textContent = brewery.name;
-          breweriesContainer.appendChild(breweryElement);
+          const card = document.createElement('div');
+          card.classList.add('brewery-card');
+      
+          // Add brewery content to the card
+          card.innerHTML = `
+            <h3>${brewery.name}</h3>
+            <p>${brewery.description}</p>
+            <!-- Add more brewery details as needed -->
+          `;
+      
+          // Append the card to the breweries list
+          breweriesList.appendChild(card);
         });
-     }
+      }
 
 
 
